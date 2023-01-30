8203b8fa6296c6d2f16a1e17a69cf3fab0ca47cd
Edward Junior Sierra Castellón <esierrac@choucairtesting.com>
2023-01-30 16:10:45 -0500

    Se agregan pruebas con funciones para arrays

---
+++ courses/class.py
@@ -0,0 +1,7 @@
+list = [7.2,2.5,3.6,4.1]
+
+print(len(list), 'Conteo de elementos')
+print(sum(list), 'Suma de todos los elementos') # Sumar los elementos de un array
+print(sorted(list, reverse = True), 'Sorted') # Los ordena en order ascendente, y el uso de la bandera para cambiar
+print(list.sort()) # Consultar para que sirve el sort
+
